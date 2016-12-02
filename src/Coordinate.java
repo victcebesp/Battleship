d01f0d1ba9021eb65736ffec6a2282bd97318c55
Victor Ceballos <victcebesp@gmail.com>
2016-12-02 21:00:15 +0000

    Refactor: multiple changes 

--- src/Coordinate.java
+++ src/Coordinate.java
@@ -1,26 +1,2 @@
 public class Coordinate {
-    private final int xPos;
-    private final int yPos;
-
-    public Coordinate(int xPos, int yPos) {
-        this.xPos = xPos;
-        this.yPos = yPos;
-    }
-
-    public int xPos(){
-        return xPos;
-    }
-
-    public int yPos(){
-        return yPos;
-    }
-
-    @Override
-    public boolean equals(Object obj){
-
-        return this.xPos() == ((Coordinate) obj).xPos() &&
-               this.yPos() == ((Coordinate) obj).yPos();
-
-
-    }
 }
